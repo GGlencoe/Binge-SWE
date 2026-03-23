5ff3de71ea350e9fb867bd741c617b8aef0efe63
Justin Zhang <justin.zhang.1@slu.edu>
2026-03-22 20:53:21 -0500

    Implemented Navigation Bar

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,20 +1,5 @@
-import SwipeDeck from "@/components/SwipeDeck"
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-orange-50 flex flex-col items-center justify-center p-8">
-
-      {/* Logo */}
-      <div>
-        <img src="/bingeLogo.png" alt="Binge" className="mt-2 h-32 w-auto"/>
-      </div>
-
-
-      <p className="mb-4 text-sm text-center bg-gradient-to-b from-blue-500 to-gray-700 bg-clip-text text-transparent">
-  <strong>Swipe</strong> ← <strong>left</strong> to skip &nbsp;|&nbsp; Swipe <strong>right</strong> → to like
-</p>
-
-      <SwipeDeck />
-    </main>
-  )
+  redirect("/recipes");
 }
---
+++ my-app/app/restaurants/page.tsx
@@ -0,0 +1,19 @@
+import SwipeDeck from "@/components/SwipeDeck"
+
+export default function ProfilePage() {
+  return (
+    <main className="min-h-screen bg-orange-50 flex flex-col items-center justify-center p-8">
+
+      {/* Logo */}
+      <div>
+        <img src="/bingeLogo.png" alt="Binge" className="mt-2 h-32 w-auto"/>
+      </div>
+
+
+      <p className="mb-4 text-sm text-center bg-gradient-to-b from-blue-500 to-gray-700 bg-clip-text text-transparent">
+Nearby Restaurants will be here! (This page is under construction)
+</p>
+
+    </main>
+  )
+}
